9a6c3e3875d2142a74c5497015cff4fb4599a7de
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-03-09 19:58:59 +0100

    Routing parameters
    #42

--- 13-blog/src/routes.js
+++ 13-blog/src/routes.js
@@ -1,9 +1,11 @@
 import showBlogs from './components/showBlogs';
 import listBlogs from './components/listBlogs';
 import addBlog from './components/addBlog';
+import singleBlog from './components/singleBlog';
 export default [
   { path: '*', component: showBlogs},
   { path: '/', component: showBlogs},
   { path: '/list', component: listBlogs},
-  { path: '/add', component: addBlog}
+  { path: '/add', component: addBlog},
+  { path: '/blog/:id', component: singleBlog}
 ]
