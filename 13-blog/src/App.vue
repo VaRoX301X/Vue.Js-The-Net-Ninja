a61517093a24a07318b2d23b0c4365f59fda3858
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-03-09 19:39:56 +0100

    Router links
    #41

--- 13-blog/src/App.vue
+++ 13-blog/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
@@ -8,6 +9,7 @@
 import addBlog from "./components/addBlog";
 import showBlogs from './components/showBlogs';
 import listBlogs from './components/listBlogs';
+import header from './components/header';
 export default {
 
   name: 'app',
@@ -19,7 +21,8 @@ export default {
   components: {
     'add-blog': addBlog,
     'show-blogs': showBlogs,
-    'list-blogs': listBlogs
+    'list-blogs': listBlogs,
+    'app-header': header
   }
 }
 </script>
