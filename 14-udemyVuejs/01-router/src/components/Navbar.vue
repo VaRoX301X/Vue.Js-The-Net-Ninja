2137a0e47585a89f28a0ac7ce9c52a2b4e9d88b7
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-03-11 23:22:08 +0100

    More on router links
    32

--- 14-udemyVuejs/01-router/src/components/Navbar.vue
+++ 14-udemyVuejs/01-router/src/components/Navbar.vue
@@ -3,13 +3,24 @@
       <ul>
         <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
         <li><router-link :to="{ name: 'About' }">About</router-link></li>
+        <h2>User profiles</h2>
+        <ul>
+          <li v-for="(id, index) in userIds" :key="index">
+            <router-link :to="{ name: 'ViewProfile', params: { user_id: id}}">Profile {{ index }}</router-link>
+          </li>
+        </ul>
       </ul>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Navbar"
+        name: "Navbar",
+      data(){
+          return {
+            userIds: ['1','2','3','4']
+          }
+      }
     }
 </script>
 
