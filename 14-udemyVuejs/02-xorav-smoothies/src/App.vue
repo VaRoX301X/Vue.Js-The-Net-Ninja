fc3bbf3babbed39db0ded358f599ec3985de1056
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-03-12 00:16:35 +0100

    Navbar Component

--- 14-udemyVuejs/02-xorav-smoothies/src/App.vue
+++ 14-udemyVuejs/02-xorav-smoothies/src/App.vue
@@ -1,50 +1,28 @@
 <template>
   <div id="app">
-      <router-view></router-view>
+    <navbar></navbar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Home from "./components/Home";
+  import Home from "./components/Home";
+  import Navbar from "./components/Navbar";
 
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'app',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    components: {
+      'home': Home,
+      'navbar': Navbar
     }
-  },
-  components: {
-    'home': Home
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
