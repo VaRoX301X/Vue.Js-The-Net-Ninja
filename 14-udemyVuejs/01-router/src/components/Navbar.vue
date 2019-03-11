65461dca770fc428eaeb24024c5fddc1e6ea6929
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-03-11 23:37:02 +0100

    Redirect progammatically
    33

--- 14-udemyVuejs/01-router/src/components/Navbar.vue
+++ 14-udemyVuejs/01-router/src/components/Navbar.vue
@@ -3,12 +3,18 @@
       <ul>
         <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
         <li><router-link :to="{ name: 'About' }">About</router-link></li>
-        <h2>User profiles</h2>
-        <ul>
-          <li v-for="(id, index) in userIds" :key="index">
-            <router-link :to="{ name: 'ViewProfile', params: { user_id: id}}">Profile {{ index }}</router-link>
-          </li>
-        </ul>
+      </ul>
+      <h2>User profiles</h2>
+      <ul>
+        <li v-for="(id, index) in userIds" :key="index">
+          <router-link :to="{ name: 'ViewProfile', params: { user_id: id}}">Profile {{ index }}</router-link>
+        </li>
+      </ul>
+      <h2>Navigation controls</h2>
+      <ul>
+        <li><button v-on:click="goBack">Back</button></li>
+        <li><button v-on:click="goHome">Redirect to Home</button></li>
+        <li><button v-on:click="goForward">Forward</button></li>
       </ul>
     </div>
 </template>
@@ -20,6 +26,17 @@
           return {
             userIds: ['1','2','3','4']
           }
+      },
+      methods: {
+          goHome() {
+            this.$router.push({ name: 'Home' })
+          },
+        goBack() {
+            this.$router.go(-1)
+        },
+        goForward() {
+            this.$router.go(+1)
+        }
       }
     }
 </script>
@@ -31,5 +48,9 @@
   }
   a {
     color: #42b983;
+  }
+  li {
+    display: inline-block;
+    margin: 10px;
   }
 </style>
