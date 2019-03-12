c9024f189156a16ba4860813d8ab74520da027fe
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-03-12 19:05:25 +0100

    Add Smoothie Component II

--- 14-udemyVuejs/02-xorav-smoothies/src/components/addSmoothie.vue
+++ 14-udemyVuejs/02-xorav-smoothies/src/components/addSmoothie.vue
@@ -8,9 +8,10 @@
         </div>
         <div class="field add-ingredient">
           <label for="add-ingredient">Add an ingredient:</label>
-          <input type="text" name="add-ingredient">
+          <input type="text" name="add-ingredient" v-on:keydown.tab.prevent="addIng" v-model="another">
         </div>
         <div class="field center-align">
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
           <button class="btn pink">Add Smoothie</button>
         </div>
       </form>
@@ -24,13 +25,25 @@
       data (){
           return {
             title: null,
-
+            another: null,
+            ingredients: [],
+            feedback: null
           }
       },
       methods:{
           AddSmoothie: function () {
             console.log(this.title);
-          }
+          },
+        addIng() {
+            if (this.another){
+              this.ingredients.push(this.another);
+              console.log(this.ingredients);
+              this.another = null;
+              this.feedback = null;
+            } else {
+              this.feedback = 'You must enter a value to add an ingredient';
+            }
+        }
       }
     }
 </script>
