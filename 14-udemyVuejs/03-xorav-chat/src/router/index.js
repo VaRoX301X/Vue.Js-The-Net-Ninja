85e4a40b70bfa80c7069c5c2d0ca56780fd55d6d
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-03-14 23:22:59 +0100

    Router Guarding ---

--- 14-udemyVuejs/03-xorav-chat/src/router/index.js
+++ 14-udemyVuejs/03-xorav-chat/src/router/index.js
@@ -7,7 +7,16 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
-    { path: '/', name: 'Welcome', component: Welcome },
-    { path: '/chat', name: 'Chat', component: Chat, props: true }
+    {path: '/', name: 'Welcome', component: Welcome},
+    { // IR A CHAT SI Y SOLO SI SE ESCRIBE NOMBRE - ROUTE GUARDING
+      path: '/chat', name: 'Chat', component: Chat, props: true, beforeEnter: (to, from, next) => {
+        console.log(to.params.name);
+        if(to.params.name){
+          next();
+        } else {
+          next({ name: 'Welcome' });
+        }
+      }
+    }
   ]
 })
