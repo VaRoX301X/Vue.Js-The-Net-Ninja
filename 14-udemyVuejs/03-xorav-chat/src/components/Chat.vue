490a201528ca2c2c717ee879523482eabc52c38a
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-03-17 16:24:52 +0100

    chat window

--- 14-udemyVuejs/03-xorav-chat/src/components/Chat.vue
+++ 14-udemyVuejs/03-xorav-chat/src/components/Chat.vue
@@ -1,7 +1,21 @@
 <template>
     <div class="chat container">
-      <h2>Chat</h2>
-      <h3>{{name}}</h3>
+      <h2 class="center teal-text">Xorav Chat</h2>
+      <div class="card">
+        <div class="card-content">
+          <ul class="messages">
+            <li class="">
+              <span class="teal-text">Name</span>
+              <span class="grey-text text-darken-3">message</span>
+              <span class="grey-text time">tine</span>
+            </li>
+          </ul>
+        </div>
+        <div class="card-action">
+          <input type="text">
+        </div>
+      </div>
+
     </div>
 </template>
 
@@ -25,5 +39,15 @@
 </script>
 
 <style scoped>
-
+  .chat h2{
+    font-size: 2.6rem;
+    margin-bottom: 2.3rem;
+  }
+  .chat span {
+    font-size: 1.4rem;
+  }
+  .chat .time {
+    display: block;
+    font-style: 1.2rem;
+  }
 </style>
