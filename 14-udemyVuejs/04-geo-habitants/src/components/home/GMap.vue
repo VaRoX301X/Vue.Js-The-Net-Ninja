5bdb1da3fbf69bcd9b70a4b422e56ec536f5a514
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-04-30 09:17:20 +0200

    google markers

--- 14-udemyVuejs/04-geo-habitants/src/components/home/GMap.vue
+++ 14-udemyVuejs/04-geo-habitants/src/components/home/GMap.vue
@@ -1,72 +1,89 @@
 <template>
-    <div class="map">
-      <div class="google-map" id="map"></div>
-    </div>
+  <div class="map">
+    <div class="google-map" id="map"></div>
+  </div>
 </template>
 
 <script>
   import firebase from 'firebase';
   import db from '../../firebase/init';
-    export default {
-        name: "GMap",
-      data() {
-          return {
-            lat: 40,
-            lng: -3
-          }
-      },
-      methods: {
-        renderMap(){
-          const map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: this.lat, lng: this.lng},
-            zoom: 6,
-            maxZoom: 15,
-            minZoom: 3,
-            streetViewControl: false
-          })
-        }
-      },
-      mounted() {
-          // get current user
-        let user = firebase.auth().currentUser;
-        console.log(user);
-          // get user geolocation
-          if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(pos => {
-              this.lat = pos.coords.latitude;
-              this.lng = pos.coords.longitude;
 
-              //find the user record and then update geocoords
-              db.collection('users').where('user_id', '==', user.uid).get()
-                .then(snapshot => {
-                  snapshot.forEach((doc) => {
-                    db.collection('users').doc(doc.id).update({
-                      geolocation: {
-                        lat: pos.coords.latitude,
-                        lng: pos.coords.longitude
-                      }
-                    })
-                  })
-                }).then(() => {
-                  this.renderMap();
-              })
-            }, (err) => {
-              console.log(err);
-              this.renderMap();
-              }, {
-              maximumAge: 60000,
-              timeout: 3000
-            })
-          } else {
-            // position centre by default values
-            this.renderMap();
-          }
-
-          console.log(firebase.auth().currentUser);
-
-
+  export default {
+    name: "GMap",
+    data() {
+      return {
+        lat: 40,
+        lng: -3
       }
+    },
+    methods: {
+      renderMap() {
+        const map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: this.lat, lng: this.lng},
+          zoom: 6,
+          maxZoom: 15,
+          minZoom: 3,
+          streetViewControl: false
+        });
+        db.collection('users').get().then(users => {
+          users.docs.forEach(doc => {
+            let data = doc.data();
+            if (data.geolocation) {
+              let marker = new google.maps.Marker({
+                position: {
+                  lat: data.geolocation.lat,
+                  lng: data.geolocation.lng
+                },
+                map
+              });
+              // add click event to marker
+              marker.addListener('click', () => {
+                console.log(doc.id);
+              })
+            }
+          })
+        })
+      }
+    },
+    mounted() {
+      // get current user
+      let user = firebase.auth().currentUser;
+      console.log(user);
+      // get user geolocation
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(pos => {
+          this.lat = pos.coords.latitude;
+          this.lng = pos.coords.longitude;
+
+          //find the user record and then update geocoords
+          db.collection('users').where('user_id', '==', user.uid).get()
+            .then(snapshot => {
+              snapshot.forEach((doc) => {
+                db.collection('users').doc(doc.id).update({
+                  geolocation: {
+                    lat: pos.coords.latitude,
+                    lng: pos.coords.longitude
+                  }
+                })
+              })
+            }).then(() => {
+            this.renderMap();
+          })
+        }, (err) => {
+          console.log(err);
+          this.renderMap();
+        }, {
+          maximumAge: 60000,
+          timeout: 3000
+        })
+      } else {
+        // position centre by default values
+        this.renderMap();
+      }
+
+
     }
+  }
 </script>
 
 <style scoped>
